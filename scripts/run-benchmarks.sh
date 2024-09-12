75d6940c63602dda24bebf12cdf2d909a62c6b2d
Andrea Lamparelli <a.lamparelli95@gmail.com>
2024-09-12 17:36:10 +0200

    Add client-side suspension mechanism
    Signed-off-by: Andrea Lamparelli <a.lamparelli95@gmail.com>

--- scripts/run-benchmarks.sh
+++ scripts/run-benchmarks.sh
@@ -7,12 +7,13 @@ CURRENT_DIR=$(dirname $0)
 #### overridable configuration ####
 ###################################
 
-TEST_CASE=${TEST_CASE:-"simple_request_300_rps"}
+TEST_CASE=${TEST_CASE:-"simple_request_100_rps"}
 
 THREADS=${THREADS:-10}
 
 WARMUP_BEFORE_PAUSE=${WARMUP_BEFORE_PAUSE:-20}
 SERVER_PAUSE_DURATION=${SERVER_PAUSE_DURATION:-7}
+CLIENT_PAUSE_DURATION=${CLIENT_PAUSE_DURATION:-0}
 
 HYPERFOIL_HOME=${HYPERFOIL_HOME:-$CURRENT_DIR/hyperfoil}
 K6=${K6:-"k6"}
@@ -53,6 +54,7 @@ if [ "$HF_ENABLED" = "true" ]; then
     source $TEST_CASE_FOLDER/hf-config.env
     ${HF} -R ${RATE} -c ${CONNECTIONS} -t ${THREADS} -d ${DURATION}s --latency ${FULL_URL} &> "$TEST_CASE_RESULTS_FOLDER/hyperfoil/hf.log" &
     wrk_pid=$!
+    echo "----- Hyperfoil started with pid ${wrk_pid}"
 fi
 
 # jmeter
@@ -60,6 +62,7 @@ if [ "$JMETER_ENABLED" = "true" ]; then
     mkdir -p "$TEST_CASE_RESULTS_FOLDER/jmeter"
     ${JMETER} -n -t $TEST_CASE_FOLDER/jmeter-config.jmx -l $TEST_CASE_RESULTS_FOLDER/jmeter/log.jtl -j $TEST_CASE_RESULTS_FOLDER/jmeter/jmeter.log -e -o $TEST_CASE_RESULTS_FOLDER/jmeter/jmeter-report -f >/dev/null &
     jmeter_pid=$!
+    echo "----- Jmeter started with pid ${jmeter_pid}"
 fi
 
 # k6
@@ -67,6 +70,7 @@ if [ "$K6_ENABLED" = "true" ]; then
     mkdir -p "$TEST_CASE_RESULTS_FOLDER/k6"
     ${K6} run --summary-trend-stats="avg,min,med,max,p(50),p(75),p(90),p(99),p(99.9),p(99.99),p(99.999),count" --out json=$TEST_CASE_RESULTS_FOLDER/k6/k6-output.json $TEST_CASE_FOLDER/k6-config.js &> "$TEST_CASE_RESULTS_FOLDER/k6/k6.log" &
     k6_pid=$!
+    echo "----- K6 started with pid ${k6_pid}"
 fi
 
 # artillery
@@ -74,17 +78,61 @@ if [ "$ARTILLERY_ENABLED" = "true" ]; then
     mkdir -p "$TEST_CASE_RESULTS_FOLDER/artillery"
     ${ARTILLERY} run $TEST_CASE_FOLDER/artillery-config.yaml -o $TEST_CASE_RESULTS_FOLDER/artillery/artillery-report.json &> "$TEST_CASE_RESULTS_FOLDER/artillery/artillery.log" &
     artillery_pid=$!
+    echo "----- Artillery started with pid ${artillery_pid}"
 fi
 
 # suspend the server
 if [ ! "$SERVER_PAUSE_DURATION" -eq "0" ]; then
     sleep $WARMUP_BEFORE_PAUSE
 
-    echo "----- Suspending the server simulating a severe stall server-side"
+    echo "----- Suspending the server for ${SERVER_PAUSE_DURATION}s"
     kill -STOP $quarkus_pid
     sleep $SERVER_PAUSE_DURATION
     echo "----- Resuming the server"
     kill -CONT $quarkus_pid
+fi
+
+# suspend the client
+if [ ! "$CLIENT_PAUSE_DURATION" -eq "0" ]; then
+    sleep $WARMUP_BEFORE_PAUSE
+
+    if [ "$HF_ENABLED" = "true" ]; then
+        echo "----- Suspending the hyperfoil load generator for ${CLIENT_PAUSE_DURATION}s"
+        hf_pid=$(ps aux | grep 'io.hyperfoil.cli.commands.Wrk2' | grep -v grep | awk '{print $2}')
+        kill -STOP $hf_pid
+    fi
+    if [ "$K6_ENABLED" = "true" ]; then
+        echo "----- Suspending the k6 load generator for ${CLIENT_PAUSE_DURATION}s"
+        kill -STOP $k6_pid
+    fi
+    if [ "$ARTILLERY_ENABLED" = "true" ]; then
+        echo "----- Suspending the artillery load generator for ${CLIENT_PAUSE_DURATION}s"
+        kill -STOP $artillery_pid
+    fi
+    if [ "$JMETER_ENABLED" = "true" ]; then
+        echo "----- Suspending the jmeter load generator for ${CLIENT_PAUSE_DURATION}s"
+        kill -STOP $jmeter_pid
+    fi
+
+    sleep $CLIENT_PAUSE_DURATION
+    echo "----- Resuming the load generators"
+
+    if [ "$HF_ENABLED" = "true" ]; then
+        echo "----- Resuming the hyperfoil load generator"
+        kill -CONT $hf_pid
+    fi
+    if [ "$K6_ENABLED" = "true" ]; then
+        echo "----- Resuming the k6 load generator"
+        kill -CONT $k6_pid
+    fi
+    if [ "$ARTILLERY_ENABLED" = "true" ]; then
+        echo "----- Resuming the artillery load generator"
+        kill -CONT $artillery_pid
+    fi
+    if [ "$JMETER_ENABLED" = "true" ]; then
+        echo "----- Resuming the jmeter load generator"
+        kill -CONT $jmeter_pid
+    fi
 fi
 
 
