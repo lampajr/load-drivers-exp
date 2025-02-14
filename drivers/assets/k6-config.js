efa508933615d4bd96704de91d94949abba48e9a
Andrea Lamparelli <a.lamparelli95@gmail.com>
2025-02-14 10:10:15 +0100

    Setup the infra using qDup
    Signed-off-by: Andrea Lamparelli <a.lamparelli95@gmail.com>

--- drivers/assets/k6-config.js
+++ drivers/assets/k6-config.js
@@ -7,26 +7,25 @@ export const options = {
       executor: 'constant-arrival-rate',
 
       // How long the test lasts
-      duration: '40s',
+      duration:'40s',
 
       // How many iterations per timeUnit
-      rate: 100,
+      rate:300,
 
       // Start `rate` iterations per second
       timeUnit: '1s',
 
-      // Pre-allocate 2 VUs before starting the test
-      preAllocatedVUs: 2,
+      // // Pre-allocate 2 VUs before starting the test
+      preAllocatedVUs:2,
 
-      // Spin up a maximum of 30 VUs to sustain the defined
-      // constant arrival rate.
-      // we must set it, put a high number
-      maxVUs: 2000,
+      // // Spin up a maximum of 30 VUs to sustain the defined
+      // // constant arrival rate.
+      maxVUs:30,
     },
   },
 };
 
 
 export default function () {
-  const response = http.get('http://localhost:8080/time/cached');
+  const response = http.get('#FULL_URL_REPLACE_ME#');
 }
